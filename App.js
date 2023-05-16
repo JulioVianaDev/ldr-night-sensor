11bd00def38c0f940188bc631186e36cf516f90b
JulioVianaDev <JulioVianaDev@gmail.com>
2023-05-16 13:58:34 -0300

    principais tags ok

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View ,Button,Linking,Image} from 'react-native';
-
+import logo from './images/gatito.webp';
+import tomate from './images/tomate.png'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Teste nas tags</Text>
+      <Text>My first app!</Text>
+      <View>
+        <Text>Olá tudo bem? </Text>
+      </View>
+      <View>
+        <Image source={logo} style={{width: 250,height:250}}/>
+        <Button
+          title="Ir para o meu github"
+          onPress={()=>{Linking.openURL('https://github.com/JulioVianaDev')}}
+        />
+      </View>
+      <View>
+        <Text>
+          Sou um desenvolvedor fullstack e estou em busca de aprender javascript
+        </Text>
+      </View>
+      <View>          
+        <Image
+          source={tomate}
+          style={{width: 250,height:250}}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +35,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f0ff',
+    backgroundColor: '#f2f',
     alignItems: 'center',
     justifyContent: 'center',
   },
