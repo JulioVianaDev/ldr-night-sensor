49c825c1017c17309b8eeae91eb2215a44b4ab0e
JulioVianaDev <JulioVianaDev@gmail.com>
2023-05-16 17:37:21 -0300

    functions

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,Button, Linking,Image } from 'react-native';
 import { styles } from './styles/styles';
+import { useState } from 'react';
 export default function App() {
+  const [logado,setLogado] = useState(false)
+  function logar() {
+    setLogado(true);
+  }
+  function deslogar() {
+    setLogado(false);
+  }
   return (
     <View style={styles.body}>
       <Text style={styles.texto}>opa</Text>
