7b05f057a8a404a83bc991c790338dfb8d500fa2
JulioVianaDev <JulioVianaDev@gmail.com>
2023-05-16 17:08:37 -0300

    flatlist finalizado

--- App.js
+++ App.js
@@ -1,36 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import {  Text, View ,Button,Linking,Image} from 'react-native';
-import logo from './images/gatito.webp';
-import tomate from './images/tomate.png'
-import { styles,styleTexto,styleImages } from './styles/styles';
+import { StyleSheet, Text, View,Button, Linking,Image } from 'react-native';
+import { styles,textos} from './styles/styles'
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.topo}>
-        <Text style={styleTexto.titulo}>My first App in RN!</Text>
-      </View>
-      <View>
-        <Text  style={styleTexto.subTitutlo}>Olá tudo bem? </Text>
-      </View>
-      <View>
-        <Image source={logo} style={styleImages.padrao}/>
-        <Button
-          title="Ir para o meu github"
-          onPress={()=>{Linking.openURL('https://github.com/JulioVianaDev')}}
-        />
-      </View>
-      <View style={styles.meio}>
-        <Text  style={styleTexto.texto}>
-          Sou um desenvolvedor fullstack e estou em busca de aprender javascript
+      <View style={styles.view1}>
+        <Text style={textos.Titulo}>
+          Texto do meu titulo
         </Text>
       </View>
-      <View>          
-        <Image
-          source={tomate}
-          style={styleImages.secundaria}
-        />
+      <View style={styles.view2}>
+        <View style={styles.view3}>
+          <Text style={textos.description}>Segunda View</Text>
+        </View>
+        <Button title='bem vindo'></Button>
       </View>
-      <StatusBar style="auto" />
     </View>
   );
 }
--- styles/styles.js
+++ styles/styles.js
@@ -1,48 +1,45 @@
 import { StyleSheet } from "react-native";
-export const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#051326',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    topo:{
-        position: 'absolute',
-        backgroundColor: '#45135e',
-        top: 40,
-        width: '100%',
-        height: 70,
-    },
-    meio:{
-        margin: 10
-    }
+
+export  const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: 'flex' ,
+    backgroundColor: '#f0ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column'
+  },
+  view1:{
+    backgroundColor: '#ff6',
+    width: '80%',
+    height: '20%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    marginBottom: 25,
+  },
+  view2:{
+    backgroundColor: '#5ff',
+    width: '80%',
+    height: '20%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+  },
+  view3:{
+    margin:10,
+    marginBottom:40,
+  }
 });
 
-export const styleTexto = StyleSheet.create({
-    titulo:{
-        fontSize: 36,
-        color: 'white',
-        marginTop: 10,
-        textAlign: 'center'
-    },
-    subTitutlo:{
-        fontSize: 24,
-        color: 'white'
-    },
-    texto:{
-        fontSize:18,
-        color: 'white',
-        textAlign:"center"
-    }
-})
-
-export const styleImages=StyleSheet.create({
-    padrao:{
-        width: 250,
-        height:250
-    } ,
-    secundaria:{
-        width: 50,
-        height:50
-    }
+export const textos = StyleSheet.create({
+  Titulo:{
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  description:{
+    fontSize: 14,
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+  }
 })
