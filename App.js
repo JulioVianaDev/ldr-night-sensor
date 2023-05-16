f7c1d139447761bc96bb8f9d6c9df05a67b4ce19
JulioVianaDev <JulioVianaDev@gmail.com>
2023-05-16 17:30:42 -0300

    ok

--- App.js
+++ App.js
@@ -1,38 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,Button, Linking,Image } from 'react-native';
-import {styles,textos,linhas} from './styles/styles';
+import { styles } from './styles/styles';
 export default function App() {
   return (
     <View style={styles.body}>
-      <View style={linhas.linha}>
-        <View style={styles.view1}>
-          <Text style={textos.description}>1</Text>
-        </View>
-        <View style={styles.view2}>
-          <Text style={textos.description}>2</Text>
-        </View>
-        <View style={styles.view3}>
-          <Text style={textos.description}>3</Text>
-        </View>
-      </View>
-      <View style={linhas.linha}>
-        <View style={styles.view4}>
-          <Text style={textos.description}>4</Text>
-        </View>
-      </View>
-      <View style={linhas.linha}>
-        <View style={styles.view5}>
-          <Text style={textos.description}>5</Text>
-        </View>
-      </View>
-      <View style={linhas.superlinha}>
-        <View style={styles.view6}>
-          <Text style={textos.description}>6</Text>
-        </View>
-        <View style={styles.view7}>
-          <Text style={textos.description}>7</Text>
-        </View>
-      </View>
-  </View>
+      <Text style={styles.texto}>opa</Text>
+    </View>
   );
 }
--- styles/styles.js
+++ styles/styles.js
@@ -1,76 +1,14 @@
-import { StyleSheet  } from "react-native";
-export const styles = StyleSheet.create({
-  body: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#f7ff',
-    alignItems: 'stretch',
-    justifyContent: 'flex-start',
-  },
-  view1: {
-    flex: 1,
-    backgroundColor: '#00ffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view2: {
-    flex: 2,
-    backgroundColor: '#ff00ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view3: {
-    flex: 3,
-    backgroundColor: '#ffff00',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view4: {
-    flex: 1,
-    backgroundColor: '#ff0000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view5: {
-    flex: 1,
-    backgroundColor: '#0fff0f',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view6: {
-    flex: 1,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view7: {
-    flex: 1,
-    backgroundColor: '#0000ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import { StyleSheet } from "react-native";
 
-export const linhas = StyleSheet.create({
-  linha: {
-    flexDirection: 'row',
-    backgroundColor: '#ffffff',
-    alignItems: 'stretch',
-    justifyContent: 'center',
-  },
-  superlinha: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#ffffff',
-    alignItems: 'stretch',
-    justifyContent: 'center',
-  },
-})
-export const textos = StyleSheet.create({
-  description: {
-    color:'#000000',
-    fontSize: 35,
-    fontStyle: 'italic',
-    margin: 10,
-}
+export const styles = StyleSheet.create({
+    body:{
+        paddingTop: 30,
+        flex: 1,
+        backgroundColor: 'black'
+    },
+    texto:{
+        textAlign: 'center',
+        color: 'red',
+        fontSize: 24
+    }
 })
