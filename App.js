a60b28d34eed3073bb16fe27026917c2472a03cc
JulioVianaDev <JulioVianaDev@gmail.com>
2023-05-16 21:39:01 -0300

    cadastro ok

--- App.js
+++ App.js
@@ -1,31 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Button, Linking,Image } from 'react-native';
-import { styles } from './styles/styles';
+import { StyleSheet, Text, View,Button, Linking,Image, TextInput } from 'react-native';
+import {styles,textos,button,input,campoCadastro} from './styles/styles'
 import { useState } from 'react';
-import tomate from './images/tomate.png'
-import gatito from './images/gatito.webp'
 export default function App() {
-  const [logado,setLogado] = useState(false)
-  function logar() {
-    setLogado(true);
+  const [name,setName]= useState('')
+  const [idade,setIdade] = useState(0)
+  const [profissao,setProfissao] = useState('')
+  const [show,setShow] = useState(false)
+  const Clicado = ()=>{
+    setShow(true)
   }
-  function deslogar() {
-    setLogado(false);
+  const ClicadoCancelar = ()=>{
+    setShow(false)
   }
   return (
     <View style={styles.body}>
-      {
-        logado
-          ?<Button title='clique aqui para deslogar' onPress={()=>deslogar()}/>
-          :<Button title='clique aqui para logar' onPress={()=>logar()}/>
-      }
-      {
-        logado 
-        ? <Image style={styles.images} source={tomate}/> 
-        :<Image  style={styles.images} source={gatito}/>
-      }
-      
-      <Text style={styles.texto}>Demonstração</Text>
+      <View style={styles.view1}>
+        <Text style={textos.description}>Cadastro de Clientes</Text>
+      </View>
+      <View style={campoCadastro.global}>
+        <Text style={textos.description}>
+          Digite seu Nome
+        </Text>
+        <TextInput
+          placeholder='ex.. Tomate'
+          style={input.input}
+          onChangeText={(value)=>setName(value)}
+        />
+        <Text style={textos.description} >
+            Digite sua Idade
+        </Text>
+        <TextInput 
+          placeholder='ex.. 22'
+          style={input.input}
+          onChangeText={(value)=>setIdade(value)}
+        />
+        <Text style={textos.description} >
+          Sua profissão
+        </Text>
+        <TextInput 
+          placeholder='ex.. Jogador de Volei'
+          style={input.input}
+          onChangeText={(value)=>setProfissao(value)}
+        />
+        <Button
+          title='Cadastre-se'
+          onPress={()=>Clicado()}
+          style={button.button}
+        />
+      </View>
+      <View>
+        {
+          show  ? 
+          <View style={campoCadastro.resultado}>
+            <Text style={textos.description} >Seu nome: {name} </Text>
+            <Text style={textos.description} >Sua idade {idade}</Text>
+            <Text style={textos.description} >Sua profissão {profissao}</Text>
+            
+            <Button
+              title='Cancel'
+              onPress={()=>ClicadoCancelar()}
+              style={button.button}
+            />
+          </View> : <View></View> 
+        }
+      </View>
     </View>
   );
 }
--- styles/styles.js
+++ styles/styles.js
@@ -1,21 +1,66 @@
 import { StyleSheet } from "react-native";
-
 export const styles = StyleSheet.create({
-    body:{
-        paddingTop: 30,
-        flex: 1,
-        backgroundColor: 'black',
-        justifyContent: 'space-around',
-        alignItems:"center"
-        
+    body: {
+      flex: 1,
+      flexDirection: 'column',
+      backgroundColor: '#f7ff',
+      alignItems: 'stretch',
+      justifyContent: 'flex-start',
     },
-    texto:{
-        textAlign: 'center',
-        color: 'red',
-        fontSize: 24
+    view1: {
+      backgroundColor: '#00ffff',
+      height: 100,
+      alignItems: 'center',
+      justifyContent: 'center',
     },
-    images:{
-        width:250,
-        height:250,
+    item:{
+      margin:10,
+      backgroundColor: '#ff2ff2',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 10,
     }
-})
+  });
+  
+  export const textos = StyleSheet.create({
+    description: {
+      color:'#000000',
+      fontSize: 35,
+      fontStyle: 'italic',
+      margin: 10,
+  }
+  })
+  export const button = StyleSheet.create({
+    button:{
+      width: 50,
+      height: 50,
+       
+    }
+  })
+  
+  export const input = StyleSheet.create({
+    input:{
+      width: 200,
+      borderWidth:1,
+      borderColor: 'purple',
+      borderRadius:5,
+      textAlign: 'center',
+      fontSize: 20,
+      marginBottom: 10
+    }
+  })
+  
+  export const campoCadastro = StyleSheet.create({
+    global:{
+      margin:50,
+      alignItems: 'center',
+      justifyContent: 'space-around'
+    },
+    resultado:{
+      backgroundColor: '#f22f',
+      margin: 15,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 10,
+    }
+  })
