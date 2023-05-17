6ffa6242fee497dfc420116341428d9db50d9dcc
JulioVianaDev <JulioVianaDev@gmail.com>
2023-05-16 21:52:08 -0300

    safe area view

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Button, Linking,Image, TextInput } from 'react-native';
-import { useState } from 'react';
+import {SafeAreaView, StyleSheet, Text, View,Button, Linking,Image, TextInput } from 'react-native';
+import { useState,useEffect } from 'react';
+import {globalStyle} from './styles/styles'
 export default function App() {
   
   return (
-    <View >
-      
-    </View>
+    <SafeAreaView  style={globalStyle.body}>
+      <Text  style={globalStyle.letter}>oi</Text>
+    </SafeAreaView>
   );
 }
--- styles/styles.js
+++ styles/styles.js
@@ -1,2 +1,12 @@
 import {StyleSheet} from 'react-native';
 
+export const globalStyle = StyleSheet.create({
+    body:{
+        flex: 1,
+        backgroundColor: 'blue',
+        paddingTop: 50,
+    },
+    letter:{
+        color: 'pink'
+    }
+})
