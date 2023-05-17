854062b2cdc4d789b5c72de10c94f4434812d200
JulioVianaDev <JulioVianaDev@gmail.com>
2023-05-16 21:20:29 -0300

    hooks ok

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,Button, Linking,Image } from 'react-native';
 import { styles } from './styles/styles';
 import { useState } from 'react';
+import tomate from './images/tomate.png'
+import gatito from './images/gatito.webp'
 export default function App() {
   const [logado,setLogado] = useState(false)
   function logar() {
@@ -12,6 +14,17 @@ export default function App() {
   }
   return (
     <View style={styles.body}>
+      {
+        logado
+          ?<Button title='clique aqui para deslogar' onPress={()=>deslogar()}/>
+          :<Button title='clique aqui para logar' onPress={()=>logar()}/>
+      }
+      {
+        logado 
+        ? <Image style={styles.images} source={tomate}/> 
+        :<Image  style={styles.images} source={gatito}/>
+      }
+      
       <Text style={styles.texto}>Demonstração</Text>
     </View>
   );
--- styles/styles.js
+++ styles/styles.js
@@ -4,11 +4,18 @@ export const styles = StyleSheet.create({
     body:{
         paddingTop: 30,
         flex: 1,
-        backgroundColor: 'black'
+        backgroundColor: 'black',
+        justifyContent: 'space-around',
+        alignItems:"center"
+        
     },
     texto:{
         textAlign: 'center',
         color: 'red',
         fontSize: 24
+    },
+    images:{
+        width:250,
+        height:250,
     }
 })
