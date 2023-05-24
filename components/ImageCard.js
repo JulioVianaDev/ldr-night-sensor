535e634c6b0e849b2f4e224efbf6f55faa21ad58
JulioVianaDev <JulioVianaDev@gmail.com>
2023-05-24 14:13:01 -0300

    identando

--- components/ImageCard.js
+++ components/ImageCard.js
@@ -3,19 +3,19 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 import {styles} from '../styles/styles'
 export  const ImageCard = ({ item, index }) => { 
     return(
-    <View>
-      <TouchableOpacity>
-        <Image
-            source={{uri: item.image}}
-            style={styles.carouselImg}
-        />
-        <Text style={styles.carouselText}>{item.title}</Text>
-        <Icon 
-          name="play-circle-outline" 
-          size={30} color="#FFF" 
-          style={styles.carouselIcon} 
-        />
-      </TouchableOpacity>
-    </View>
-  );
+        <View>
+        <TouchableOpacity>
+            <Image
+                source={{uri: item.image}}
+                style={styles.carouselImg}
+            />
+            <Text style={styles.carouselText}>{item.title}</Text>
+            <Icon 
+            name="play-circle-outline" 
+            size={30} color="#FFF" 
+            style={styles.carouselIcon} 
+            />
+        </TouchableOpacity>
+        </View>
+    );
   };
